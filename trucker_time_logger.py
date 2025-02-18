2abf47ecc13c28b387d39ed69414b088ea584952
EricAyala21 <108314765+EricAyala21@users.noreply.github.com>
2025-02-18 11:13:32 -0800

    Multiple pages added

--- trucker_time_logger.py
+++ trucker_time_logger.py
@@ -16,13 +16,14 @@ class file_manage:
         customtkinter.set_appearance_mode("light")
         customtkinter.set_default_color_theme("blue")
         self.file_Names = []
+        self. frame = ""
         self.setup_gui()
 
     def setup_gui(self):
         
-        frame = customtkinter.CTkFrame(master=root)
+        self.frame = customtkinter.CTkFrame(master=root)
 
-        frame.pack(pady =20,padx = 60, fill = "both", expand = True)
+        self.frame.pack(pady =20,padx = 60, fill = "both", expand = True)
 
 
 
@@ -30,18 +31,18 @@ class file_manage:
 
   
 
-        button1 = customtkinter.CTkButton(master = frame,
+        button1 = customtkinter.CTkButton(master = self.frame,
                                           text = "Daily Log",
-                                          command = self.login,
+                                          command = self.DailyLog,
                                           height = 50,
                                           
                                           width = 120,
                                           corner_radius = 25)
         button1.grid (row = 1, column = 0, padx = 10, pady = 10)
 
-        button2 = customtkinter.CTkButton(master = frame,
+        button2 = customtkinter.CTkButton(master = self.frame,
                                           text = "View",
-                                          command = self.login,
+                                          command = self.view,
                                           height = 50,
                                           width = 120,
                                           fg_color = "#d7c5db",
@@ -49,9 +50,9 @@ class file_manage:
         button2.grid (row = 1, column = 2, padx = 10, pady = 10)
 
 
-        button3 = customtkinter.CTkButton(master = frame,
+        button3 = customtkinter.CTkButton(master = self.frame,
                                           text = "Analytics",
-                                          command = self.login,
+                                          command = self.Analytics,
                                           height = 50,
                                           width = 120,
                                           corner_radius = 25)
@@ -61,9 +62,124 @@ class file_manage:
     def login(self):
         print("Hello")
     
-        
-        
+    def view(self):
 
+        self.clearWindow()
+
+
+
+     
+
+  
+
+        button1 = customtkinter.CTkButton(master = self.frame,
+                                          text = "Daily Log",
+                                          command = self.DailyLog,
+                                          height = 10,
+                                          
+                                          width = 120,
+                                          corner_radius = 25)
+        button1.grid (row = 1, column = 0, padx = 10, pady = 10)
+
+        button2 = customtkinter.CTkButton(master = self.frame,
+                                          text = "View",
+                                          command = self.login,
+                                          height = 10,
+                                          width = 120,
+                                          fg_color = "#d7c5db",
+                                          corner_radius = 25)
+        button2.grid (row = 1, column = 2, padx = 10, pady = 10)
+
+
+        button3 = customtkinter.CTkButton(master = self.frame,
+                                          text = "Analytics",
+                                          command = self.Analytics,
+                                          height = 10,
+                                          width = 120,
+                                          corner_radius = 25)
+        button3.grid (row = 1, column = 3, padx = 10, pady = 10)
+
+
+    def Analytics(self):
+        self.clearWindow()
+
+
+
+     
+
+  
+
+        button1 = customtkinter.CTkButton(master = self.frame,
+                                          text = "Daily Log",
+                                          command = self.DailyLog,
+                                          height = 50,
+                                          
+                                          width = 120,
+                                          corner_radius = 25)
+        button1.grid (row = 1, column = 0, padx = 10, pady = 10)
+
+        button2 = customtkinter.CTkButton(master = self.frame,
+                                          text = "View",
+                                          command = self.view,
+                                          height = 50,
+                                          width = 120,
+                                          fg_color = "#d7c5db",
+                                          corner_radius = 25)
+        button2.grid (row = 1, column = 2, padx = 10, pady = 10)
+
+
+        button3 = customtkinter.CTkButton(master = self.frame,
+                                          text = "Analytics",
+                                          command = self.login,
+                                          height = 50,
+                                          width = 120,
+                                          corner_radius = 25)
+        button3.grid (row = 1, column = 3, padx = 10, pady = 10)
+
+
+    def DailyLog(self):
+        self.clearWindow()
+
+
+
+
+     
+
+  
+
+        button1 = customtkinter.CTkButton(master = self.frame,
+                                          text = "Daily Log",
+                                          command = self.login,
+                                          height = 20,
+                                          
+                                          width = 120,
+                                          corner_radius = 25)
+        button1.grid (row = 1, column = 0, padx = 10, pady = 10)
+
+        button2 = customtkinter.CTkButton(master = self.frame,
+                                          text = "View",
+                                          command = self.view,
+                                          height = 20,
+                                          width = 120,
+                                          fg_color = "#d7c5db",
+                                          corner_radius = 25)
+        button2.grid (row = 1, column = 2, padx = 10, pady = 10)
+
+
+        button3 = customtkinter.CTkButton(master = self.frame,
+                                          text = "Analytics",
+                                          command = self.Analytics,
+                                          height = 20,
+                                          width = 120,
+                                          corner_radius = 25)
+        button3.grid (row = 1, column = 3, padx = 10, pady = 10)
+
+        
+        
+    def clearWindow(self):
+        for widgets in self.frame.winfo_children():
+            widgets.destroy()
+            
 root = customtkinter.CTk()
 app = file_manage(root)
 root.mainloop()
