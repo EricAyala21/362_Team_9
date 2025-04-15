08347843d7bc5bdfe1fe43b2397d49709650dc8e
EricAyala21 <108314765+EricAyala21@users.noreply.github.com>
2025-04-14 22:28:58 -0700

    Added a better UI for daily Log, added some functions to test the data, and added a temp analytics page

--- custom_methods.py
+++ custom_methods.py
@@ -1,5 +1,5 @@
 import datetime
-
+import re
 FORMAT_STR = "%Y/%m/%d %H:%M"
 
 def str_to_datetime(s: str):
@@ -24,3 +24,23 @@ def datetime_to_str(d: datetime.datetime):
         string: fomatted string
     """
     return d.strftime(FORMAT_STR)
+
+def checkDate(date):
+    form = re.compile("^[0-9]{4}/[0-1][0-9]/[0-3][0-9]$") #is establishing the xxxx/xx/xx format
+    if form.match(date): #checks if the date matches the given format
+        return True
+    else:
+        return False
+
+def checkTime(time):
+    form = re.compile("^[0-2][0-9]:[0-6][0-9]$")# is establishing the time format xx:xx
+    if form.match(time):
+        return True
+    else:
+        return False
+    
+def checkTimeInputs(input):
+    if isinstance(input,(int,float)): #checks whether the resting time or driving time is a float or a integer 
+        return True
+    else:
+        return False
--- daily_log.py
+++ daily_log.py
@@ -2,6 +2,7 @@ import customtkinter
 from time import strftime
 import tkinter
 import sqlite3
+import custom_methods
 
 class Daily_Log(customtkinter.CTkFrame):
     def __init__(self, master, filename):
@@ -24,7 +25,7 @@ class Daily_Log(customtkinter.CTkFrame):
     def upperDisplay(self):
         # will hold the current time
         self.time_string = strftime('%H:%M')
-        self.date_string = strftime('%D')
+        self.date_string = strftime('%Y/%m/%d')
 
         self.logDetail = customtkinter.CTkFrame(self)
 
@@ -38,25 +39,25 @@ class Daily_Log(customtkinter.CTkFrame):
 
 
         #label for Driving time
-        self.DTLabel = customtkinter.CTkLabel(self.logDetail, text="Driving Time",font = ("arial",18),justify = "center",padx = 50,pady=10)
+        self.DTLabel = customtkinter.CTkLabel(self.logDetail, text="Driving Time",font = ("Helvetica",18),justify = "center",padx = 50,pady=10)
         self.DTLabel.grid(row=0, column=0, padx=5, sticky="s")
         #Input box for driving time
         self.DTInput = customtkinter.CTkEntry(self.logDetail,width = 250,height = 20)
-        self.DTInput.grid(row=1, column=0, padx=5,pady=5, sticky="n")
+        self.DTInput.grid(row=1, column=0, padx=2,pady=5, sticky="n")
         #Label for Resting Time
-        self.RTLabel = customtkinter.CTkLabel(self.logDetail, text="Resting Time",font = ("arial",18),justify = "center",padx = 50,pady=10)
+        self.RTLabel = customtkinter.CTkLabel(self.logDetail, text="Resting Time",font = ("v",18),justify = "center",padx = 50,pady=10)
         self.RTLabel.grid(row=2, column=0,  padx=5, sticky="s")
         #Text box for resting Time
         self.RTInput = customtkinter.CTkEntry(self.logDetail,width = 250,height = 20)
         self.RTInput.grid(row=3, column=0,  padx=2, sticky="n")
-        self.RTInput.grid(row=3, column=0,  padx=2,pady = 5, sticky="n")
 
         
-
+ 
         #save Botton and check box
-        self.saveButton = customtkinter.CTkButton(self.logDetail, text = "Save",command = lambda:self.save(),height=20,width=100)
+        self.saveButton = customtkinter.CTkButton(self.logDetail, text = "Save",command = lambda:self.save())
         self.saveButton.grid(column = 0, row =4 ,padx = 10, pady = 5, sticky = "ns")
-  
+        self.logDetail.bind('<Configure>',self.resizeUpperDisplay)
+
 
 
 
@@ -74,23 +75,23 @@ class Daily_Log(customtkinter.CTkFrame):
         self.customTime  =customtkinter.CTkLabel(self.logTime, text= "Time",font = ("arial",20),justify = "center", padx = 30,pady = 10)
         self.customTime.grid(row = 0,column=1,padx=(2,0),pady=2, sticky = "ns")
 
-        self.timeInput = customtkinter.CTkEntry(self.logTime)
+        self.timeInput = customtkinter.CTkEntry(self.logTime,justify = "center")
         self.timeInput.grid(row=1, column=1,  padx=(10,0),pady = (0,0), sticky="n")
         self.current_time()#gets the time in the entry box
         
         #Label and input for Date
-        self.customDate  =customtkinter.CTkLabel(self.logTime, text= "Date",font = ("arial",20),justify = "center", padx = 55,pady = 10)
-        self.customDate.grid(row = 2,column=1,padx=10,pady=(20,0), sticky = "nse")
+        self.customDate  =customtkinter.CTkLabel(self.logTime, text= "Date",font = ("arial",20),justify = "center", padx = 30,pady = 10)
+        self.customDate.grid(row = 2,column=1,padx=10,pady=(20,0), sticky = "ns")
 
 
-        self.dateInput = customtkinter.CTkEntry(self.logTime)
+        self.dateInput = customtkinter.CTkEntry(self.logTime,justify = "center")
         self.dateInput.grid(row=3, column=1,  padx=(10,0),pady = 2, sticky="s")
         self.current_day()#gets the date in the entrybox
 
         #check box for a custom time
-        self.checkBox = customtkinter.CTkCheckBox(self.logTime,text  ="Custom Time",width = 10, height = 10,command =self.checked)
+        self.checkBox = customtkinter.CTkCheckBox(self.logTime,text  ="Custom Time",width = 10, height = 10,command = self.checked)
         self.checkBox.grid(row = 4, column  =1, padx=10,pady=(10,0),sticky = "ns")
-
+        self.logTime.bind('<Configure>',self.resizeTimeView)
     
 
     # gets the time and sets the state of the entry to readonly
@@ -101,28 +102,71 @@ class Daily_Log(customtkinter.CTkFrame):
     def current_day(self):
         self.dateInput.insert(0, self.date_string)
         self.dateInput.configure(state="readonly")
-            
 
+    #Will check if the time is in the right format and will output an error if it is wrong
+    def checkTimes(self):
+        self.TInput = self.timeInput.get()
+  
+        if custom_methods.checkTime(self.TInput) == True:
+            return True
+        else:
+            return False
+   
+    def checkDates(self):
+        self.DInput = self.dateInput.get()
 
-                
+        if custom_methods.checkDate(self.DInput) == True:
+            return True
+        else:
+            return False
 
-                
+        
+    def checkDInput(self):
+        self.dData = self.DTInput.get()
+
+        if custom_methods.checkTimeInputs(self.dData) == True:
+            return True
+        else:
+            return False
+    
+    def checkRInput(self):
+        self.RData = self.RTInput.get()
+        if custom_methods.checkTimeInputs(self.RData) == True:
+            return True
+        else:
+            return False
+        
     #will check if the box is checked so it can allow the user to edit the text boxs for time and date
-    def checked(self):
-        if self.checkBox.cget("state") == "disabled":
+    def checked(self):#once the check box is in the unchecked position 
+        if self.checkBox.get() == 0:
             self.timeInput.configure(state = "disabled")
             self.dateInput.configure(state = "disabled")
+            print(self.checkBox.get())
         else:
             self.dateInput.configure(state = "normal")
             self.timeInput.configure(state = "normal")
 
+
     #will save the information into the viewpage array
     def save(self):
-        self.cur.execute("INSERT OR IGNORE INTO logs VALUES (?, ?, ?)", ["05/27/2021 05:28", 9, 8])
-        self.con.commit()
-        print("entry list after save: ", end='')
-        print(self.cur.execute("SELECT datetime FROM logs").fetchall())
-        self.deleteInputs()
+        
+
+
+        
+
+        if (self.checkDates() or self.checkDInput() or self.checkRInput() or self.checkTimes()) == True:
+            self.cur.execute("INSERT OR IGNORE INTO logs VALUES (?, ?, ?)", ["word", 2,3])
+            self.con.commit()
+            print("entry list after save: ", end='')
+            print(self.cur.execute("SELECT datetime FROM logs").fetchall())
+            self.deleteInputs()
+            self.current_time()
+            self.current_day()
+            self.checkBox.deselect()
+        else:
+            print("Error there is one wrong input")
+
+        
     #deletes the data from the input boxes whether it is in a disabled and a enabled mode    
     def deleteInputs(self):
         if self.timeInput.cget("state") == "readonly":
@@ -140,7 +184,27 @@ class Daily_Log(customtkinter.CTkFrame):
             self.dateInput.delete(0,customtkinter.END)
             self.DTInput.delete(0,customtkinter.END)
             self.RTInput.delete(0,customtkinter.END)  
+# Resizes the first window based off of the size of the application window
 
+    def resizeTimeView(self,e):
+        widgetWidth= e.width
+        widgetHeight = e.height
+        average = widgetWidth * widgetHeight
+        self.timeInput.configure(height=int(widgetHeight) /5,width = int(widgetWidth)/1.5,font = ("Helvetica",int(widgetWidth/14)))
+        self.dateInput.configure(height=int(widgetHeight) /5,width = int(widgetWidth)/1.5,font = ("Helvetica",int(widgetWidth/14)))
+        self.customTime.configure(font=("Helvetica",int(widgetWidth)/12))
+        self.customDate.configure(font=("Helvetica",int(widgetWidth)/12))
+
+# Resizes the first window based off of the size of the application window
+    def resizeUpperDisplay(self,e):
+        widgetWidth = e.width
+        widgetHeight = e.height
+
+        self.DTInput.configure(height =int(widgetHeight)/5,font = ("Helvetica",int(widgetWidth/14)),width = int(widgetWidth)/1.5)
+        self.RTInput.configure(height = int(widgetHeight)/5,font = ("Helvetica",int(widgetWidth/14)),width = int(widgetWidth)/1.5)
+        self.DTLabel.configure(font=("Helvetica", int(widgetWidth)/16))
+        self.RTLabel.configure(font=("Helvetica", int(widgetWidth)/16))
+        self.saveButton.configure(width=int(widgetWidth)/2)
 
 
 
@@ -151,4 +215,3 @@ class Daily_Log(customtkinter.CTkFrame):
 
 
         
-
