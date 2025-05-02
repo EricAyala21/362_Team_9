9e4c7ad6de69b0ffed35e2140b0859214c5c3b24
EricAyala21 <108314765+EricAyala21@users.noreply.github.com>
2025-05-01 23:32:43 -0700

    Added a basic user interface for a user to choose which save file they want to use or create, will imporove on it later, also added error popups in the daily Log page as well

--- daily_log.py
+++ daily_log.py
@@ -25,7 +25,7 @@ class Daily_Log(customtkinter.CTkFrame):
     def upperDisplay(self):
         # will hold the current time
         self.time_string = strftime('%H:%M')
-        self.date_string = strftime('%Y/%m/%d')
+        self.date_string = strftime('%m/%d/%Y')
 
         self.logDetail = customtkinter.CTkFrame(self)
 
@@ -45,7 +45,7 @@ class Daily_Log(customtkinter.CTkFrame):
         self.DTInput = customtkinter.CTkEntry(self.logDetail,width = 250,height = 20)
         self.DTInput.grid(row=1, column=0, padx=2,pady=5, sticky="n")
         #Label for Resting Time
-        self.RTLabel = customtkinter.CTkLabel(self.logDetail, text="Resting Time",font = ("v",18),justify = "center",padx = 50,pady=10)
+        self.RTLabel = customtkinter.CTkLabel(self.logDetail, text="Resting Time",font = ("Helvetica",18),justify = "center",padx = 50,pady=10)
         self.RTLabel.grid(row=2, column=0,  padx=5, sticky="s")
         #Text box for resting Time
         self.RTInput = customtkinter.CTkEntry(self.logDetail,width = 250,height = 20)
@@ -110,7 +110,12 @@ class Daily_Log(customtkinter.CTkFrame):
         if custom_methods.checkTime(self.TInput) == True:
             return True
         else:
-            return False
+            print("Theres an error In DrivingTime")
+            self.errorTime =customtkinter.CTkToplevel(self)  # master argument is optional  
+            self.errorTime.title("Error")
+            self.errorTimeLabel = customtkinter.CTkLabel(self.errorTime,text = "Error please input in the xx:xx format")
+            self.errorTimeLabel.grid(row = 2, column = 2, padx =10, pady =10, sticky = "news")
+            self.errorTime.geometry("250x40")
    
     def checkDates(self):
         self.DInput = self.dateInput.get()
@@ -118,23 +123,40 @@ class Daily_Log(customtkinter.CTkFrame):
         if custom_methods.checkDate(self.DInput) == True:
             return True
         else:
-            return False
+            print("Theres an error In DrivingTime")
+            self.errorDates =customtkinter.CTkToplevel(self)  # master argument is optional  
+            self.errorDates.title("Error")
+            self.errorDatesLabel = customtkinter.CTkLabel(self.errorDates,text = "Error please input in the xx/xx/xxxx format")
+            self.errorDatesLabel.grid(row = 2, column = 2, padx =10, pady =10, sticky = "news")
+            self.errorDates.geometry("250x40")
 
         
     def checkDInput(self):
         self.dData = self.DTInput.get()
-
+    
         if custom_methods.checkTimeInputs(self.dData) == True:
             return True
         else:
-            return False
+            print("Theres an error In DrivingTime")
+            self.errorDrive =customtkinter.CTkToplevel(self)  # master argument is optional  
+            self.errorDrive.title("Error")
+            self.errorDriveLabel = customtkinter.CTkLabel(self.errorDrive,text = "Error please input a numerical value")
+            self.errorDriveLabel.grid(row = 2, column = 2, padx =10, pady =10, sticky = "news")
+            self.errorDrive.geometry("250x40")
     
+
+
     def checkRInput(self):
         self.RData = self.RTInput.get()
         if custom_methods.checkTimeInputs(self.RData) == True:
             return True
         else:
-            return False
+            print("Theres an error In DrivingTime")
+            self.errorRest =customtkinter.CTkToplevel(self)  # master argument is optional  
+            self.errorRest.title("Error")
+            self.errorRestLabel = customtkinter.CTkLabel(self.errorRest,text = "Error please input a numerical value")
+            self.errorRestLabel.grid(row = 2, column = 2, padx =10, pady =10, sticky = "news")
+            self.errorRest.geometry("250x40")
         
     #will check if the box is checked so it can allow the user to edit the text boxs for time and date
     def checked(self):#once the check box is in the unchecked position 
@@ -151,18 +173,23 @@ class Daily_Log(customtkinter.CTkFrame):
     def save(self):
         
 
-
         
 
-        if (self.checkDates() or self.checkDInput() or self.checkRInput() or self.checkTimes()) == True:
-            self.cur.execute("INSERT OR IGNORE INTO logs VALUES (?, ?, ?)", ["word", 2,3])
+        if self.checkDates() and self.checkDInput() and self.checkRInput() and self.checkTimes():
+            self.timestamp = self.DInput + " "+ self.TInput
+            self.logTempEntries = [[self.timestamp,float(self.DTInput.get()),float(self.RTInput.get())]]
+            print(self.timestamp)
+            #self.cur.execute("DELETE FROM logs")  # Delete all rows
+            self.cur.executemany("INSERT OR IGNORE INTO logs VALUES (?, ?, ?)", self.logTempEntries)
             self.con.commit()
             print("entry list after save: ", end='')
-            print(self.cur.execute("SELECT datetime FROM logs").fetchall())
+            print(self.cur.execute("SELECT timestamp FROM logs").fetchall())
             self.deleteInputs()
             self.current_time()
             self.current_day()
             self.checkBox.deselect()
+            self.current_time()
+            self.current_day()
         else:
             print("Error there is one wrong input")
 
--- trucker_time_logger.py
+++ trucker_time_logger.py
@@ -2,207 +2,40 @@ import customtkinter
 from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
-import daily_log
-import viewpage
-import sqlite3
-import Analytics
 
+import fileSelector
 
 
 class file_manage:
     def __init__(self, root):
         self.root = root
         self.root.geometry("650x350")
-        self.root.minsize(630, 300)
+        self.root.minsize(630, 300) 
         customtkinter.set_appearance_mode("light")
         customtkinter.set_default_color_theme("blue")
         
-        self.file_Names = []
         self.con = None # sql connection
         self.cur = None # sql cursor
-        self.setup_data()
 
-        self.frame = ""
         self.setup_gui()
 
 
-    def setup_data(self):
-        #TODO look through available data file / ask user for file input
-        # temporary database for test and development purpose
-        self.file_Names = ["temp.db", "test.db"]
-        self.con = sqlite3.connect(self.file_Names[0])
-        self.cur = self.con.cursor()
-        # create a table named logs if not exists. datetime set to primary key for fast lookup
-        self.cur.execute(f"""CREATE TABLE IF NOT EXISTS logs(
-                         timestamp TEXT PRIMARY KEY, 
-                         drivetime FLOAT, 
-                         resttime FLOAT)""")
-        # create an index for logs based on the datetime to enhance sorting
-        self.cur.execute("CREATE INDEX IF NOT EXISTS idx_timestamp ON logs(timestamp)")
 
-        # starting entries for test only
-        # store the entries data we have
-        self.log_entries=[["2025/02/26 12:35", 12, 10],
-                         ["2025/02/25 09:16", 8, 9],
-                         ["2025/02/24 22:33", 10, 12],
-                         ["2025/01/18 08:43", 5, 7],
-                         ["2024/12/07 18:00", 12, 9],
-                         ["2024/12/01 16:26", 8, 5],
-                         ["2024/11/30 17:53", 8, 13],
-                         ["2024/10/13 15:00", 5, 10],
-                         ["2024/09/06 11:27", 13, 9],
-                         ["2024/05/27 06:28", 8, 9]]
-        self.cur.executemany("INSERT OR IGNORE INTO logs VALUES(?, ?, ?)", self.log_entries)
-        self.con.commit()
+
 
     def setup_gui(self):
+        self.page = fileSelector.Users(master = root)
+        self.page.pack(pady = 20, padx= 60,fill = "both", expand = True )
+        self.page.grid_columnconfigure((0,1,2),weight = 1)
+        self.page.grid_rowconfigure(1,weight = 1)
+        self.page.grid_rowconfigure(2,weight = 6)
         
-        self.frame = customtkinter.CTkFrame(master=root)
 
-        self.frame.pack(pady =20,padx = 60, fill = "both", expand = True)
-        self.frame.grid_columnconfigure((0,1,2), weight=1)
-        self.frame.grid_rowconfigure(1, weight=1)
-        self.frame.grid_rowconfigure(2, weight=6)
-     
+        
 
-  
-
-        button1 = customtkinter.CTkButton(master = self.frame,
-                                          text = "Daily Log",
-                                          command = self.DailyLog,
-                                          height = 50,
-                                          width = 120,
-                                          corner_radius = 25)
-        button1.grid (row = 1, column = 0, padx = 10, pady = 10)
-
-        button2 = customtkinter.CTkButton(master = self.frame,
-                                          text = "View",
-                                          command = self.view,
-                                          height = 50,
-                                          width = 120,
-                                          corner_radius = 25)
-        button2.grid (row = 1, column = 1, padx = 10, pady = 10)
-
-
-        button3 = customtkinter.CTkButton(master = self.frame,
-                                          text = "Analytics",
-                                          command = self.Analytics,
-                                          height = 50,
-                                          width = 120,
-                                          corner_radius = 25)
-        button3.grid (row = 1, column = 2, padx = 10, pady = 10)
-
-
-    def login(self):
-        print("Hello")
     
-    def view(self):
-
-        self.clearWindow()
-
-  
-
-        button1 = customtkinter.CTkButton(master = self.frame,
-                                          text = "Daily Log",
-                                          command = self.DailyLog,
-                                          height = 30,
-                                          width = 120,
-                                          corner_radius = 25)
-        button1.grid (row = 1, column = 0, padx = 10, pady = (10, 5))
-
-        button2 = customtkinter.CTkButton(master = self.frame,
-                                          text = "View",
-                                          command = self.login,
-                                          height = 30,
-                                          width = 120,
-                                          fg_color = "#d7c5db",
-                                          corner_radius = 25)
-        button2.grid (row = 1, column = 1, padx = 10, pady = (10, 5))
 
 
-        button3 = customtkinter.CTkButton(master = self.frame,
-                                          text = "Analytics",
-                                          command = self.Analytics,
-                                          height = 30,
-                                          width = 120,
-                                          corner_radius = 25)
-        button3.grid (row = 1, column = 2, padx = 10, pady = (10, 5))
-
-        page = viewpage.ViewPage(self.frame, self.file_Names[1]) # TODO maybe can change to the selected file?
-        page.grid(row=2, column=0, columnspan=3, padx = 10, pady = (0,10), sticky="nswe")
-
-
-    def Analytics(self):
-        self.clearWindow()
-
-
-  
-        button1 = customtkinter.CTkButton(master = self.frame,
-                                          text = "Daily Log",
-                                          command = self.DailyLog,
-                                          height = 30,
-                                          width = 120,
-                                          corner_radius = 25)
-        button1.grid (row = 1, column = 0, padx = 10, pady = (10, 5))
-
-        button2 = customtkinter.CTkButton(master = self.frame,
-                                          text = "View",
-                                          command = self.view,
-                                          height = 30,
-                                          width = 120,
-                                          corner_radius = 25)
-        button2.grid (row = 1, column = 1, padx = 10, pady = (10, 5))
-
-
-        button3 = customtkinter.CTkButton(master = self.frame,
-                                          text = "Analytics",
-                                          command = self.login,
-                                          height = 30,
-                                          width = 120,
-                                          fg_color = "#d7c5db",
-                                          corner_radius = 25)
-        button3.grid (row = 1, column = 2, padx = 10, pady = (10, 5))
-        page = Analytics.AnalyticsPage(self.frame, self.file_Names[1]) # TODO maybe can change to the selected file?
-        page.grid(row=3, column=0, columnspan=3, padx = 10, pady = (0,10), sticky="nswe")
-
-    def DailyLog(self):
-        self.clearWindow()
-     
-
-
-        button1 = customtkinter.CTkButton(master = self.frame,
-                                          text = "Daily Log",
-                                          command = self.login,
-                                          height = 30,
-                                          width = 120,
-                                          fg_color = "#d7c5db",
-                                          corner_radius = 25)
-        button1.grid (row = 1, column = 0, padx = 10, pady = 10)
-
-        button2 = customtkinter.CTkButton(master = self.frame,
-                                          text = "View",
-                                          command = self.view,
-                                          height = 30,
-                                          width = 120,
-                                          corner_radius = 25)
-        button2.grid (row = 1, column = 1, padx = 10, pady = 10)
-
-
-        button3 = customtkinter.CTkButton(master = self.frame,
-                                          text = "Analytics",
-                                          command = self.Analytics,
-                                          height = 30,
-                                          width = 120,
-                                          corner_radius = 25)
-        button3.grid (row = 1, column = 2, padx = 10, pady = 10)
-        page = daily_log.Daily_Log(self.frame, self.file_Names[0]) # temporarily use the first file
-        page.grid(row=2, column=0, columnspan=3, padx = 10, pady = (0,10), sticky="nswe")
-
-        
-        
-    def clearWindow(self):
-        for widgets in self.frame.winfo_children():
-            widgets.destroy()
             
 root = customtkinter.CTk()
 app = file_manage(root)
